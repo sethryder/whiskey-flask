95a2c897542d816702853ce8b257a75a071b9ca4
Seth Ryder <ryder.seth@gmail.com>
2011-03-29 18:35:22 -0400

    Added the buildOptions function, and some bug fixes

--- whiskeyFlask.php
+++ whiskeyFlask.php
@@ -3,7 +3,7 @@
 /**
  * @author Seth Ryder
  * @name Whiskey Flask
- * @version 0.01
+ * @version 0.02
  */
 
 class whiskeyFlask {
@@ -12,10 +12,11 @@ class whiskeyFlask {
     var $api_url;
     var $format;
     
-    function __construct($site, $key, $format) {
-        
+    //Build up some required settings.
+    function __construct($site, $key, $format='json') {        
 	   $this->api_key = $key;
        
+        //Switch our API url depending on what API we are using.
         switch($site) {
             case "cv":
                 $this->api_url = 'http://api.comicvine.com';
@@ -24,6 +25,7 @@ class whiskeyFlask {
                 $this->api_url = 'http://api.giantbomb.com';
         }
 
+        //Define how we want our results returned with either XML or JSON.
         switch($format) {
             case "xml":
                 $this->format = 'xml';
@@ -33,7 +35,8 @@ class whiskeyFlask {
                 break;
         }
     }
-        
+    
+    //Function used for a detail resource request.    
     function getDetail($resource, $id, $field_list=NULL) {
         $rawData = file_get_contents("$this->api_url/$resource/$id/?api_key=$this->api_key&field_list=$field_list&format=$this->format");
 
@@ -47,8 +50,13 @@ class whiskeyFlask {
         return $result;
     }
     
+    //Function used for a list resource request.
     function getList($resource, $offset=0, $limit=100, $field_list=NULL) {
-        $rawData = file_get_contents("$this->api_url/$resource/?api_key=$this->api_key&offset=$offset&field_list=$field_list&format=$this->format");
+        $options = $this->buildOptions('list', array($field_list));
+        
+        print_r($options);
+        
+        $rawData = file_get_contents("$this->api_url/$resource/?api_key=$this->api_key&offset=$offset&$options&format=$this->format");
         
         if($this->format == 'json') {
             $results = json_decode($rawData);
@@ -60,8 +68,11 @@ class whiskeyFlask {
         return $results;
     }
     
-    function search($query, $resources=NULL, $offset=0, $limit=20, $field_list='') {
-        $rawData = file_get_contents("$this->api_url/search/?api_key=$this->api_key&query=$query&resources=$resources&offset=$offset&format=$this->format");
+    //Function used for searching.
+    function search($query, $resources=NULL, $offset=0, $limit=20, $field_list=NULL) {
+        $options = $this->buildOptions('search', array($resources,$field_list));
+        
+        $rawData = file_get_contents("$this->api_url/search/?api_key=$this->api_key&query=$query&$options&offset=$offset&format=$this->format");
         
         if($this->format == 'json') {
             $results = json_decode($rawData);
@@ -69,8 +80,40 @@ class whiskeyFlask {
         elseif ($this->format == 'xml') {
             $results = simplexml_load_string($rawData);
         }
-        
+                
         return $results;
+    }
+    
+    //This function is used to build our optons for pulls when needed. Some filters do not like to be leaved blank, which is part of the reason this is required.
+    function buildOptions($resource, $options) {        
+        switch($resource) {
+            case "search":
+                if ($options[0] == NULL) {    
+                    $rOptions = '';
+                } else {
+                    $rOptions = "resources=$options[0]&";
+                }
+                if ($options[1] == NULL) {    
+                    $fOptions = '';
+                } else {
+                    $fOptions = "field_list=$options[1]&";
+                }
+                $oReturn = $rOptions.$fOptions;
+                return $oReturn;
+                break;
+            case "list":
+                if ($options[0] == NULL) {    
+                    $fOptions = '';
+                } else {
+                    $fOptions = "field_list=$options[0]&";
+                }
+                return $fOptions;
+                break;
+            /* At the moment this seems to not be needed.
+            case "detail":
+                break; 
+            */
+        }  
     }
 }
 
